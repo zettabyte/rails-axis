b017510239b864764e0b5545407536ebcf6c6026
Kendall Gifford <kendall@titlemanagers.com>
2011-06-06 14:15:17 -0600

    Restructured and added before_filter placeholder.

--- lib/axis/controller.rb
+++ lib/axis/controller.rb
@@ -1,9 +1,6 @@
 # vim: fileencoding=utf-8:
 module Axis
   module Controller
-    def self.included(base)
-      base.send(:extend, ClassMethods)
-    end
     module ClassMethods
 
       #
@@ -20,5 +17,41 @@ module Axis
       end
 
     end
+
+    #
+    # Grab a state (or collection of state) instance(s) associated with the
+    # current controller/action pair, optionally selecting a specific one from
+    # the heirarchy using the provided parameter(s) as selectors.
+    #
+    # NOTE: This method is _noisy_! It raises ArgumentError if you pass
+    #       parameter(s) that DO NOT correspond/select an actual binding!
+    #
+    # Returns nil of the selected binding has no state instance associated with
+    # it yet. Otherwise it either returns a state instance, or a Hash (a
+    # collection, possibly hierarchy) of state instances nested at and/or below
+    # the current selection point.
+    #
+    def axis_state(*args)
+    end
+
+    ############################################################################
+    private
+    ############################################################################
+
+    def axis_before_filter
+      # determine controller and action pairing, load all any/all bindings for pair
+      # grab any state objects from session that relate to any of these bindings (or their descendants)
+      # initialize any new state instances for any/all bindings that don't already have one and store ref. in session
+      # parse out each group of passed parameters dealing with any binding: .each do |params|
+      #   update associated state based on passed params
+      # end
+    end
+
+    def self.included(base)
+      base.before_filter :axis_before_filter
+      base.helper_method :axis_state
+      base.extend ClassMethods
+    end
+
   end
 end
