7582c12c12e3b6085d22ee52e4e42d9a82c3b28d
Kendall Gifford <kendall@titlemanagers.com>
2011-06-07 11:25:20 -0600

    Completed transition to rails engine.
    By having first-class view helpers and view templates, there's no need
    to extend ActionView anymore.

--- lib/axis/engine.rb
+++ lib/axis/engine.rb
@@ -4,10 +4,21 @@ require 'rails'
 
 module Axis
   class Engine < Rails::Engine
+
+    #
+    # Location of our view helpers and view templates...
+    #
+    paths.app.helpers = "lib/axis/rails/helpers"
+    paths.app.views   = "lib/axis/rails/views"
+
+    #
+    # Extend ActiveRecord::Base and ActionController::Base with our axis macro
+    # methods...
+    #
     initializer 'rails-axis' do |app|
       ActiveSupport.on_load(:active_record)     { include Axis::Model      }
-      ActiveSupport.on_load(:action_view)       { include Axis::View       }
       ActiveSupport.on_load(:action_controller) { include Axis::Controller }
     end
+
   end
 end
