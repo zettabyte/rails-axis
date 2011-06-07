d1886e46b764ae1dc8ca5fbbf1d03dc721eef614
Kendall Gifford <kendall@titlemanagers.com>
2011-06-07 11:33:00 -0600

    Completed transition to using helper class.

--- lib/axis/rails/helpers/axis_helper.rb
+++ lib/axis/rails/helpers/axis_helper.rb
@@ -1,15 +1,11 @@
 # vim: fileencoding=utf-8:
-module Axis
-  module View
+module AxisHelper
 
-    #
-    #
-    def axis_table(*args)
-      options = args.extract_options!
-      binding = Binding.load(controller_name, action_name, *args)
-      raise ArgumentError, "invalid selectors" unless binding
-      "<table border='0' cellpadding='0' cellspacing='0'></table>".html_safe
-    end
+  def axis_table(*args)
+    options = args.extract_options!
+    binding = Binding.load(controller_name, action_name, *args)
+    raise ArgumentError, "invalid selectors" unless binding
+    "<table border='0' cellpadding='0' cellspacing='0'></table>".html_safe
+  end
 
-  end # module Model
-end   # module Axis
+end
