acc19c0994f3a0a2e25660ccb27ea6cc6340a382
Kendall Gifford <kendall@titlemanagers.com>
2011-06-16 18:17:22 -0600

    Reworked the axis before_filter.

--- lib/axis/controller.rb
+++ lib/axis/controller.rb
@@ -23,64 +23,37 @@ module Axis
     ############################################################################
 
     def axis_before_filter
-      # Don't do anything unless this is a GET request with axis data
-      return unless method == :get and params["_axis"]
-      controller = self.class
-      action     = action_name
-      options    = params.delete("_axis")
-
-      # See if any filter modifications made...
-      if options.is_a?(String) and options =~ /\A\d+\z/
-        binding = Binding[options.to_i]
-        raise "invalid binding id (controller doesn't match): #{options}" unless binding.controller == controller
-        raise "invalid binding id (action doesn't match): #{options}"     unless binding.action     == action
-        # check for a reset, then for a filter-delete, then for a filter-add, then for a filter-update
-        case params["commit"]
-        when "Reset"
-          # TODO: nuke all existing filters
-        when "Delete"
-          # TODO: delete specified filter
-        when "Add"
-          # TODO: add a new filter (check for per-page change(s))
-        when "Update"
-          # TODO: update all filters' values (including per-page change(s))
-        else
-          raise "invalid axis request: #{params["commit"]}" if params["commit"]
-        end
-        # TODO: recompute list of SQL filters, detecting any logical change(s)
-        # TODO: if any filter changes made, reset page, selected, total, pages, etc...
-        # TODO: if any changes made to page and/or selected, reset child binding's page/selected recursively...
+      #
+      # Initialize a state instance for each binding on the form
+      #
+      @axis        = {}.with_indifferent_access # main axis interface hash
+      axis_session = session["axis"] ||= {}
+      bindings     = Binding.assoc(self.class, action_name)
+      bindings.each do |b|
+        axis_session[b.id] = State.new(b.id) unless axis_session[b.id]
+        @axis[b.handle] = @axis[b.id] = axis_session[b.id]
       end
 
-      Binding.assoc(controller, action).each do |binding|
-        state = axis_binding_state(binding.id)
-        if params["_axis"] and params["_axis"][binding.id]
-          options = params["_axis"].delete(binding.id)
-          #
-          # per-page change  --> update pages (reset page/selected if changed)
-          # order change(s)  --> reset page/selected if changed
-          # page selection   --> update selected if changed
-          # selection change --> done!
-          #
-        end
+      #
+      # Don't do anything more unless this is a request with axis updates
+      #
+      options = params.delete("axis")
+      return unless options.is_a?(Hash)
+
+      #
+      # Process any binding update data in binding-hierarchical order
+      #
+      queue = Binding.root(self.class, action_name)
+      until queue.empty?
+        binding = queue.shift      # get next binding to be processed...
+        queue  += binding.children # add direct children to end of queue...
+        @axis[b.id].update(options[binding.id]) # update the state
       end
-    end
-
-    def axis_session
-      session["axis"] ||= {}
-    end
-
-    def axis_binding_state(binding_id)
-      raise ArgumentError, "invalid type for binding_id: #{binding_id.class} (#{binding_id})" unless binding_id.is_a?(Fixnum)
-      raise ArgumentError, "invalid value for binding_id: #{binding_id}" unless (0...Binding.count).member?(binding_id)
-      raise ArgumentError, "invalid binding_id (binding refers to different controller): #{binding_id}" unless Binding[binding_id].controller == self.class
-      raise ArgumentError, "invalid binding_id (binding refers to different action): #{binding_id}" unless Binding[binding_id].action == action_name
-      axis_session[binding_id] ||= State.new(binding_id, 0, 25, 0, 1, nil, [], [])
     end
 
     def self.included(base)
       base.before_filter :axis_before_filter
-      base.helper        :axis
+      base.helper Axis::UrlHelper, Axis::GuiHelper
       base.extend ClassMethods
     end
 
